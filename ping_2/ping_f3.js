9542de08f9662f9c8e4ed163f031689f6bb1b0cb
WilliamVisentini <80109839+WilliamVisentini@users.noreply.github.com>
2024-11-01 11:02:11 -0300

    Add files via upload

---
+++ ping_2/ping_f3.js
@@ -0,0 +1,84 @@
+const ping = require('ping');
+
+// Função para converter uma string de endereço IP em um array de octetos
+function ipToArray(ip) {
+    return ip.split('.').map(Number);
+}
+
+// Função para converter um array de octetos de volta em uma string de endereço IP
+function arrayToIp(arr) {
+    return arr.join('.');
+}
+
+// Função para incrementar um endereço IP em um
+function incrementIp(ipArray) {
+    for (let i = ipArray.length - 1; i >= 0; i--) {
+        if (ipArray[i] < 255) {
+            ipArray[i]++;
+            return ipArray;
+        } else {
+            ipArray[i] = 0;
+        }
+    }
+    return ipArray;
+}
+
+// Função para pingar todos os IPs em um intervalo
+async function pingRange(startIp, endIp) {
+    let currentIp = ipToArray(startIp);
+    const finalIp = ipToArray(endIp);
+    const pingResults = [];
+
+    while (arrayToIp(currentIp) !== arrayToIp(finalIp)) {
+        const ip = arrayToIp(currentIp);
+        pingResults.push(
+            (async () => {
+                try {
+                    const res = await ping.promise.probe(ip);
+                    return {
+                        ip: ip,
+                        alive: res.alive,
+                    };
+                } catch (error) {
+                    console.error(`Erro ao pingar ${ip}:`, error);
+                    return {
+                        ip: ip,
+                        alive: false,
+                    };
+                }
+            })()
+        );
+
+        // Incrementa para o próximo IP
+        incrementIp(currentIp);
+    }
+
+    // Aguarda todas as promessas de ping serem resolvidas
+    const results = await Promise.all(pingResults);
+
+    // Ordena os resultados pela ordem dos IPs
+    results.sort((a, b) => {
+        const ipA = ipToArray(a.ip);
+        const ipB = ipToArray(b.ip);
+        return ipA.reduce((acc, curr, idx) => acc || (curr - ipB[idx]), 0);
+    });
+
+    // Exibe os resultados em ordem
+    results.forEach(result => {
+        if (result.alive) {
+            console.log('\x1b[38;5;214m%s\x1b[0m', `Host ${result.ip} está online!`); // Laranja claro
+        } else {
+            console.log('\x1b[38;5;250m%s\x1b[0m', `Host ${result.ip} não foi alcançado.`); // Cinza
+        }
+    });
+
+    console.log('FIM');
+}
+
+// Defina o intervalo de IP
+const startIp0 = '192.168.3.0';
+const endIp0 = '192.168.3.255';
+
+console.log('INÍCIO');
+
+pingRange(startIp0, endIp0);
---
+++ ping_2/run.bat
@@ -0,0 +1,7 @@
+@echo off
+cd "C:\Users\Path"
+node ping_f0.js
+node ping_f1.js
+node ping_f2.js
+node ping_f3.js
+pause
